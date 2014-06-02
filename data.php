ebdf89ae507d7720f6d0bbad5fbefc8bd4949e10
CiklopLT <juska.karolis@gmail.com>
2014-06-02 15:46:56 +0300

    Delete data.php

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include("connectDB.php");
-
-$query = "SELECT Slapyvardis FROM totalizatorius WHERE Vardas = 'Jonas'";
-$seek = mysql_query($query) or die(mysql_error());
-$row = mysql_fetch_array($seek);
-
-echo $row[0];
-?>
