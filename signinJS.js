39bc08879cdf9011137d0b3f6624825ee729fcdd
CiklopLT <juska.karolis@gmail.com>
2014-06-05 16:47:43 +0300

    Update signinJS.js

--- signinJS.js
+++ signinJS.js
@@ -4,6 +4,64 @@ function onBodyLoad()
 }function onDeviceReady()
 {
 
+	form = $("#main");
+
+    $("#main").validate({ 
+        rules: {
+            user: {
+                required: true
+            },
+			pass: {
+                required: true
+            },
+			pass2: {
+				required: true,
+      			equalTo: "#pass"
+    		},
+			name: {
+                required: true
+            },
+			surname: {
+                required: true
+            },
+			email: {
+                required: true,
+                email: true
+            }			
+        },
+		
+		 messages: {
+    		user: {
+      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>"
+    				},
+			pass: {
+      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>"				
+    				},
+			pass2: {
+      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>",
+				equalTo: "<span style='font-size:12px; color:#CCC'>  Slaptažodžių nesutapimas</span>"
+    				},
+			name: {
+      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>"
+    				},
+			surname: {
+      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>"
+    				},
+    		email: {
+      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>",
+      			email: "<span style='font-size:12px; color:#CCC'>  Netinkamas formatas</span>"
+    				}
+					
+  			},
+		
+        submitHandler: function ( form ) { 
+           $.post( "http://pogo.lt/total/new_reg.php" , $("#main").serialize()).done (function( data ) { 
+		});
+        }
+		
+    });
+
+
 
 $("#signin_btn").mousedown(function() {
 
@@ -92,67 +150,8 @@ $("#back_bets").click(function() {
 	});			
 
 
-$(document).ready(function () {
-	
-	
-	form = $("#main");
 
-    $("#main").validate({ 
-        rules: {
-            user: {
-                required: true
-            },
-			pass: {
-                required: true
-            },
-			pass2: {
-				required: true,
-      			equalTo: "#pass"
-    		},
-			name: {
-                required: true
-            },
-			surname: {
-                required: true
-            },
-			email: {
-                required: true,
-                email: true
-            }			
-        },
-		
-		 messages: {
-    		user: {
-      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>"
-    				},
-			pass: {
-      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>"				
-    				},
-			pass2: {
-      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>",
-				equalTo: "<span style='font-size:12px; color:#CCC'>  Slaptažodžių nesutapimas</span>"
-    				},
-			name: {
-      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>"
-    				},
-			surname: {
-      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>"
-    				},
-    		email: {
-      			required: "<span style='font-size:12px; color:#CCC'>  Privalomas laukas</span>",
-      			email: "<span style='font-size:12px; color:#CCC'>  Netinkamas formatas</span>"
-    				}
-					
-  			},
-		
-        submitHandler: function ( form ) { 
-           $.post( "http://pogo.lt/total/new_reg.php" , $("#main").serialize()).done (function( data ) { 
-		});
-        }
-		
-    });
 
-});
 
 
 $("#register_proceed").click(function() {
