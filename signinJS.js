ac3f0f7ddfad2e0b5a0c14f6a1daf3d581b119c2
CiklopLT <juska.karolis@gmail.com>
2014-06-06 13:12:09 +0300

    Update signinJS.js

--- signinJS.js
+++ signinJS.js
@@ -3,12 +3,12 @@ function onBodyLoad()
     document.addEventListener("deviceready", onDeviceReady, false);
 }function onDeviceReady()
 {
-	
+	/*
 	document.addEventListener("offline", yourCallbackFunction, false);
 	function onOffline() {
     		alert ("Reikalinga interneto prieiga");
-	}
-
+	}*/
+	
 $("#signin_btn").mousedown(function() {
 
 	var user = $("#user_signin").val();
